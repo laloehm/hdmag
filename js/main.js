1b34ccc7fd8152fd8b2cccc6d4b4088e6e3bea59
laloehm <laloehm1@gmail.com>
2019-10-24 18:28:24 -0500

    Animacion icono

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 const autoras = $('.autoras');
 
 autoras.on('click', '.header', function () {
-    $(this).siblings().slideToggle().parent().toggleClass('active').find('.hide').css('transform', 'rotate(90deg)');
-    $(this).parent().siblings().find('.body').slideUp().parent().removeClass('active').find('.hide').css('transform', 'rotate(0deg)');
-
-    // $(this).parent().removeClass('active');
-    // $(this).parent().css('background', '#fff');
-    // $(this).parent().addClass('active');
-    // $(this).parent().siblings().css('background', '#160812');
+    $(this).siblings().slideToggle().parent().toggleClass('active').find('.hide').toggleClass('h-active');
+    $(this).parent().siblings().find('.body').slideUp().parent().removeClass('active').find('.hide').removeClass('h-active');
 })
 
 console.log(autoras);
